2690f148512b0284d404722d046bd78ed991aa94
saurabhg1117 <saurabhg1117@gmail.com>
2023-07-29 14:58:25 +0530

    Flipkart Application

---
+++ src/main/java/org/example/OrderStatus.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public enum OrderStatus {
+    CREATED,PENDING, UNSHIPPED, SHIPPED, CANCELLED, COMPLETED;
+}
---
+++ src/main/java/org/example/Searchable.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public interface Searchable {
+    public ArrayList<Product> searchProduct(String productName);
+    public ArrayList<Product> searchCategory(String categoryName);
+}
+
